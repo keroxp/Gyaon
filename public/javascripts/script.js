55fc35ecfd41cc1861b32caca225980acf125185
Yusuke Sakurai <yusuke.sakurai@linecorp.com>
2016-05-17 19:20:01 +0900

    fix

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,30 +1,24 @@
-$(function(){		
-		var audioContext = new AudioContext;
-		var recorder = new Recorder(audioContext);
+$(function() {
+    var audioContext = new AudioContext();
+    var recorder = new Recorder(audioContext);
 
-		$('#rec').mousedown(function(e){
-			recorder.recStart();
-			console.log("a");
-			});
-		$('#rec').mouseup(function(e){
-			recorder.recStop();
-			var src = audioContext.createBufferSource();
-			src.buffer = recorder.getAudioBuffer();
-			src.connect(audioContext.destination);
-			src.start();
-			});
+    $('#rec').mousedown(function(e) {
+        recorder.recStart();
+        console.log("a");
+    }).mouseup(function(e) {
+        recorder.recStop();
+        var src = audioContext.createBufferSource();
+        src.buffer = recorder.getAudioBuffer();
+        src.connect(audioContext.destination);
+        src.start();
+    });
 
-		//TODO jQueryでかく
-		var memos = document.getElementsByClassName("memo");
-		for(var i = 0; i < memos.length; i++){
-		memos[i].onmouseenter = function(e){
-		this.children[0].play();
-		this.setAttribute("id", "playng");
-		}
-		memos[i].onmouseout = function(e){
-			this.children[0].pause();
-			this.children[0].currentTime = 0;
-			this.removeAttribute("id");
-		}
-		}
+    $(".memo").on("mouseenter", function(e) {
+        this.children[0].play();
+        this.dataset.playing = true;
+    }).on("mouseout", function(e) {
+        this.children[0].pause();
+        this.children[0].currentTime = 0;
+				delete this.dataset.playing;
+    });
 });
